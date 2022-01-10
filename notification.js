16f19f01f237994b42f6f6f082cb3a97ae12e7eb
huxiubin <xiubin.hu@rootcloud.com>
2022-01-10 16:47:18 +0800

    chore: ci

---
+++ notification.js
@@ -0,0 +1,93 @@
+const request = require('request')
+var path = require('path')
+const fs = require('fs')
+const md5 = require('md5')
+const exec = require('child_process').exec
+const WEB_HOOK = process.env.WECOM_WEBHOOK_KEY
+const projectName = process.env.PROJECT_NAME
+
+function getImgParams() {
+  const previewImgPath = path.join('./preview.png')
+
+  return new Promise((resolve) => {
+    const content = fs.readFileSync(previewImgPath)
+    const md5Code = md5(content)
+    // const base64 = Base64.encode(content);
+    const base64 = Buffer.from(content, 'binary').toString('base64')
+    resolve({ md5Code, base64 })
+  })
+}
+
+function noticeMsg() {
+  const newDate = new Date()
+  return new Promise((resolve, reject) => {
+    exec('git log --oneline -1', (error, stdout) => {
+      if (error) {
+        console.error('error: ' + error)
+        return reject()
+      }
+      const data = {
+        msgtype: 'markdown',
+        markdown: {
+          content: `
+          >项目名称：${projectName}
+          >最新的提交commitId和记录：${stdout}
+          >发布人：<font color="error">github action</font>
+          >发布时间：${newDate}
+          >二维码失效时间： ${new Date(
+            newDate.setMinutes(newDate.getMinutes() + 30)
+          )}`
+        }
+      }
+      request.post(
+        WEB_HOOK,
+        {
+          body: JSON.stringify(data),
+          headers: {
+            'Content-Type': 'application/json'
+          }
+        },
+        function (err) {
+          if (err) {
+            console.error(err)
+            return reject()
+          }
+          resolve()
+        }
+      )
+    })
+  })
+}
+
+async function noticeQCode() {
+  try {
+    await noticeMsg()
+    const { md5Code, base64 } = await getImgParams()
+    console.log(md5Code)
+    const data = {
+      msgtype: 'image',
+      image: {
+        base64,
+        md5: md5Code
+      }
+    }
+    request.post(
+      WEB_HOOK,
+      {
+        body: JSON.stringify(data),
+        headers: {
+          'Content-Type': 'application/json'
+        }
+      },
+      function (err, resp, body) {
+        if (err) {
+          console.error(err)
+        }
+      }
+    )
+  } catch (error) {
+    console.log('err')
+  }
+}
+
+noticeQCode()
---
+++ shell/checkout.sh
@@ -0,0 +1,55 @@
+
+#!/bin/bash
+
+set -e
+
+repositoryUrl="${GITLAB_REPO_URL}"
+username="${GITLAB_REPO_USERNAME}"
+password="${GITLAB_REPO_PASSWORD}"
+branchName=${1}
+devBranch='dev'
+testBranch='test'
+masterBranch='master'
+prodBranch='prod'
+
+function log() {
+  echo "$(date)>>>>$@"
+}
+
+# 登录
+
+echo git config --global user.name $username
+echo git config --global user.email $password
+echo git config --global credential.helper store
+
+# 克隆分支代码
+
+if [[ $branchName == $testBranch ]];then
+    echo "包含test"
+    git clone -b test $repositoryUrl
+elif [[ $branchName == $masterBranch ]];then
+    echo "包含master"
+    git clone $repositoryUrl
+elif [[ $branchName == $prodBranch ]];then
+    echo "包含prod"
+    git clone -b prod $repositoryUrl
+elif [[ $branchName == $devBranch ]];then
+    echo "包含dev"
+    git clone -b dev $repositoryUrl
+else
+    echo "默认执行test分支代码"
+    git clone -b test $repositoryUrl
+fi
+
+# cd g-crm-app 
+# log "$(git branch)"
+# # 拉取最新代码
+# git pull
+# cd ..  
+
+# 将代码放到github runner 执行目录下
+cd g-miniprogram && mv * ../ 
+
+pwd
+
+ls -l
